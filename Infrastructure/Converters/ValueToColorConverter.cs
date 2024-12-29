c2baf747295604da3d918765369426c993817cf6
Ihor Terniuk <ihor.terniuk1990@gmail.com>
2024-12-30 00:12:04 +0200

    Created GameBoard. Added some styles and commands.

---
+++ Infrastructure/Converters/ValueToColorConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Game2048.Infrastructure.Converters
+{
+    internal class ValueToColorConverter : IValueConverter
+    {
+        #region Brushes
+        private static readonly SolidColorBrush tile2Brush = GetSolidColorBrush(192, 192, 192);
+        private static readonly SolidColorBrush tile4Brush = GetSolidColorBrush(169, 169, 169);
+        private static readonly SolidColorBrush tile8Brush = GetSolidColorBrush(100, 149, 237);
+        private static readonly SolidColorBrush tile16Brush = GetSolidColorBrush(65, 105, 255);
+        private static readonly SolidColorBrush tile32Brush = GetSolidColorBrush(0, 0, 255);
+        private static readonly SolidColorBrush tile64Brush = GetSolidColorBrush(0, 255, 0);
+        private static readonly SolidColorBrush tile128Brush = GetSolidColorBrush(50, 205, 50);
+        private static readonly SolidColorBrush tile256Brush = GetSolidColorBrush(0, 128, 0);
+        private static readonly SolidColorBrush tile512Brush = GetSolidColorBrush(255, 165, 0);
+        private static readonly SolidColorBrush tile1024Brush = GetSolidColorBrush(255, 140, 0);
+        private static readonly SolidColorBrush tile2048Brush = GetSolidColorBrush(255, 69, 0);
+        private static readonly SolidColorBrush tileEmptyBrush = GetSolidColorBrush(217, 174, 125);
+        #endregion
+
+        private static readonly Dictionary<string, Brush> BrushDictionary = new()
+        {
+            {"2", tile2Brush},
+            {"4", tile4Brush},
+            {"8", tile8Brush},
+            {"16", tile16Brush},
+            {"32", tile32Brush},
+            {"64", tile64Brush},
+            {"128", tile128Brush},
+            {"256", tile256Brush},
+            {"512", tile512Brush},
+            {"1024", tile1024Brush},
+            {"2048", tile2048Brush},
+        };
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (BrushDictionary.TryGetValue(value as string, out var brush))
+                return brush;
+            else
+                return tileEmptyBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static SolidColorBrush GetSolidColorBrush(byte r = 0, byte g = 0 , byte b = 0, byte a = 255)
+        {
+            return new SolidColorBrush(GetColor(r, g, b, a));
+        }
+
+        private static System.Windows.Media.Color GetColor(byte r = 0, byte g = 0, byte b = 0, byte a = 255)
+        {
+            return new System.Windows.Media.Color { R = r, G = g, B = b, A = a }; 
+        }
+    }
+}
---
+++ ViewModels/GameViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Game2048.Infrastructure.Commands;
+using Game2048.Models;
+using Game2048.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Game2048.ViewModels
+{
+    internal class GameViewModel : ViewModel
+    {
+        #region Fields
+        
+        private GameBoard _gameBoard;
+        private Random _random;
+        private readonly int _probabilityOfFour = 90;
+        private readonly int maxIter = 5000;
+
+        #endregion
+
+        #region Properties
+        public int[,] Board { get => _gameBoard.Board; set=> Set(ref _gameBoard.board, value); }
+
+        public int Score {  get => _gameBoard.Score; set => Set(ref _gameBoard.score, value); }
+        #endregion
+
+        #region Contructors
+        public GameViewModel()
+        {
+            Init();
+        }
+        #endregion
+
+        #region Commands
+        public NavigationCommand NavigateToMenuPageCommand { get => new(NavigateToPage, new Uri("Views/Pages/MainMenuPage.xaml", UriKind.RelativeOrAbsolute)); }
+        public ICommand ResetCommand { get; }
+
+        private bool CanResetCommandExecuted(object p) => true;
+
+        private void OnResetCommandExecuted(object p)
+        {
+            Reset();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void Init()
+        {
+            _gameBoard = new GameBoard();
+            _random = new Random();
+            Reset();
+        }
+        private void Reset()
+        {
+            Board = new int[_gameBoard.BoardSize, _gameBoard.BoardSize];
+            SetTileOnRandomPosition();
+            SetTileOnRandomPosition();
+            Update();
+        }
+
+        private void SetTileOnRandomPosition()
+        {
+            int row, col;
+            
+            int iter = 0;
+            do
+            {
+                row = _random.Next(_gameBoard.BoardSize);
+                col = _random.Next(_gameBoard.BoardSize);
+            } while (_gameBoard.board[row, col] != 0 && iter++ < maxIter);
+
+            _gameBoard.board[row, col] = GenerateRandomTileValue();
+        }
+
+        private int GenerateRandomTileValue()
+        {
+            return _random.Next(100) < _probabilityOfFour ? 2 : 4;
+        }
+
+        private void Update()
+        {
+            Board = _gameBoard.board;
+            Score = _gameBoard.score;
+        }
+
+        #endregion Methods
+    }
+}
